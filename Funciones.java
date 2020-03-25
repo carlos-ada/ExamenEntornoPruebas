2e64f9c142eb6e66171473f7bb298335d0451da3
carlos-ada <carlos.villar.web@adaits.es>
2020-03-25 20:36:51 +0100

    Funcion 4

--- Funciones.java
+++ Funciones.java
@@ -68,6 +68,27 @@ public class Funcion3 {
 		// TODO Auto-generated method stub
 		return null;
 	}
+public class Funcion4 {
+	public static void main(String[] args) {
 
+
+		String[] CadenaNombres = { "Javier Melendez", "Carlos Garcia", "Carlos Villar", "Juan Jesus" };
+
+		System.out.println(Funcion4(CadenaNombres));
+
+        }
+		public static String Funcion4(String[] CadenaNombres) {
+	
+			String CadenaConcatenada = null;
+			for (int i = 0; i < CadenaNombres.length; i++) {
+	
+				CadenaConcatenada = CadenaNombres[0] + "-" + CadenaNombres[1] + "-" + CadenaNombres[2] + "-"
+						+ CadenaNombres[3];
+			}
+			CadenaConcatenada = CadenaConcatenada.replaceAll("[AaEeIiOoUu]", "");
+			CadenaConcatenada = CadenaConcatenada.replaceAll("[A-Z]", "");
+			System.out.println(CadenaConcatenada);
+			return CadenaConcatenada;
+		}
 
 }
