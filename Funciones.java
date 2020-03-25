e83761b7a3cd5fc1d4bcf2f7cba5e34a0dcc3162
carlos-ada <carlos.villar.web@adaits.es>
2020-03-25 20:31:40 +0100

    Funcion 3

--- Funciones.java
+++ Funciones.java
@@ -45,6 +45,29 @@ public class Funcion2 {
 		// TODO Auto-generated method stub
 		return null;
 	}
-}
+public class Funcion3 {
+
+	public static int Funcion3(int num) {
+
+
+		int x = 5;
+		int ymenos = 200-50;
+		int ymas = 200+50;
+		int rang = (int)Math.floor(Math.random()*(ymas+ymenos));
+		int result = 0;
+
+		if (num%x == 0) {
+			result = rang%num;
+		}
+
+		return result;
+
+	}
+
+	public Integer funcion3(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
