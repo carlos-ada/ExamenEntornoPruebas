2ac12d0535ccae57aaf2d4724b090a2cb7b24ec8
carlos-ada <carlos.villar.web@adaits.es>
2020-03-25 20:39:56 +0100

    Funcion 5

--- Funciones.java
+++ Funciones.java
@@ -90,5 +90,21 @@ public class Funcion4 {
 			System.out.println(CadenaConcatenada);
 			return CadenaConcatenada;
 		}
+public class Funcion5{
+
+
+	public  int Funcion5(int [] num) {
+
+		int tam = num.length;
+		int sum = 0; 
+		int [] numarray = new int [tam];
+
+		for (int cont = 0; cont < tam; cont++) {
+			numarray[cont] = num[cont];
+			sum = sum + numarray[cont];
+		}
+		return sum;
+	}
+}
 
 }
